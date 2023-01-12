30cd7f03a68bca65d95e4f3d2846836110a9014c
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2023-01-11 22:41:56 -0500

    Fix various bugs and create "logo"

--- modules/configuration-menu/Menu.lua
+++ modules/configuration-menu/Menu.lua
@@ -5,6 +5,21 @@ local _G = _G
 local frame = CreateFrame("Frame", nil)
 frame.name = addonName
 
+function setDefaultText(frame)
+    frame:SetText("Player-Server (ex: Philouxo-Area52)")
+end
+
+function hideFrames(...)
+    for key,value in pairs({...}) do
+        value:Hide()
+    end
+end
+
+function showFrames(...)
+    for key,value in pairs({...}) do
+        value:Show()
+    end
+end
 
 -- populate frame
 frame:SetScript("OnShow",
@@ -13,18 +28,95 @@ frame:SetScript("OnShow",
         title:SetPoint("TOPLEFT", 16, -16)
         title:SetText(addonName)
 
+        -- list of players/channels
+        PlayerWhitelistingDropdown = CreateFrame("Frame",
+                                                              "WhitelistDropDown",
+                                                              frame,
+                                                              "UIDropDownMenuTemplate")
+        PlayerWhitelistingDropdown:SetPoint("BOTTOMLEFT")
+        UIDropDownMenu_SetWidth(PlayerWhitelistingDropdown, 200)
+        UIDropDownMenu_SetText(PlayerWhitelistingDropdown, "Whitelisted players")
+        UIDropDownMenu_Initialize(PlayerWhitelistingDropdown, addon.populatePlayersDropDown)
+
+        ChannelWhitelistingDropdown = CreateFrame("Frame",
+                                "WhitelistDropDown",
+                                frame,
+                                "UIDropDownMenuTemplate")
+        ChannelWhitelistingDropdown:SetPoint("BOTTOMRIGHT")
+        UIDropDownMenu_SetWidth(ChannelWhitelistingDropdown, 200)
+        UIDropDownMenu_SetText(ChannelWhitelistingDropdown, "Whitelisted channels")
+        UIDropDownMenu_Initialize(ChannelWhitelistingDropdown, addon.populateDropDownChannels)
+
+        -- add/remove players
+        AddWhitelistPlayerEditBox = CreateFrame("EditBox",
+                                         "EditBoxFrame",
+                                         frame,
+                                         "InputBoxTemplate")
+        AddWhitelistPlayerEditBox:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 16, -65)
+        AddWhitelistPlayerEditBox:SetSize(200, 300)
+        AddWhitelistPlayerEditBox:SetAutoFocus(false)
+        setDefaultText(AddWhitelistPlayerEditBox)
+
+        AddWhitelistPlayerEditBox:SetScript("OnEditFocusGained", function(self)
+            AddWhitelistPlayerEditBox:SetText("")
+        end)
+
+        AddButton = CreateFrame("Button",
+                                       "AddPlayerButton",
+                                       frame,
+                                       "UIPanelButtonTemplate")
+        AddButton:SetSize(80 ,22)
+        AddButton:SetText("Add player")
+        AddButton:SetPoint("CENTER", AddWhitelistPlayerEditBox, "CENTER", 200, 0)
+        
+        AddButton:SetScript("OnClick", function(self)
+            local user = AddWhitelistPlayerEditBox:GetText()
+
+            if (not _G[addonName]["WhitelistedPlayers"][user]) then
+                _G[addonName]["WhitelistedPlayers"][user] = true
+                UIDropDownMenu_Initialize(PlayerWhitelistingDropdown, addon.populatePlayersDropDown)
+            end
+            AddWhitelistPlayerEditBox:SetText("")
+        end)
+
+
+        RemoveButton = CreateFrame("Button",
+                                          "RemovePlayerButton",
+                                          frame,
+                                          "UIPanelButtonTemplate")
+        RemoveButton:SetSize(80 ,22)
+        RemoveButton:SetText("Remove player")
+        RemoveButton:SetPoint("CENTER", AddWhitelistPlayerEditBox, "CENTER", 300, 0)
+
+        RemoveButton:SetScript("OnClick", function(self)
+            local user = AddWhitelistPlayerEditBox:GetText()
+
+            if (_G[addonName]["WhitelistedPlayers"][user]) then
+                _G[addonName]["WhitelistedPlayers"][user] = nil
+                UIDropDownMenu_Initialize(PlayerWhitelistingDropdown, addon.populatePlayersDropDown)
+            end
+            setDefaultText(AddWhitelistPlayerEditBox)
+        end)
+
         -- checkboxes enable/disable
         local player_whitelisting = CreateFrame("CheckButton",
                                                 "PlayerWhitelisting",
                                                 frame,
-                                                "ChatConfigCheckButtonTemplate")
+                                                    "ChatConfigCheckButtonTemplate")
         player_whitelisting:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 16, -16)
         PlayerWhitelistingText:SetText("Toggle player whitelisting")
         player_whitelisting.tooltip = "Enable/Disable whitelisting of players"
         player_whitelisting:SetChecked(_G[addonName]["WhitelistPlayers"])
         player_whitelisting:SetScript("OnClick", 
             function(self)
+                local isChecked = self:GetChecked()
                 _G[addonName]["WhitelistPlayers"] = PlayerWhitelisting:GetChecked()
+                
+                if isChecked then
+                    showFrames(AddButton, RemoveButton, AddWhitelistPlayerEditBox, PlayerWhitelistingDropdown)
+                else
+                    hideFrames(AddButton, RemoveButton, AddWhitelistPlayerEditBox, PlayerWhitelistingDropdown)
+                end
             end
         )
 
@@ -38,30 +130,18 @@ frame:SetScript("OnShow",
         channel_whitelisting:SetChecked(_G[addonName]["WhitelistChannels"])
         channel_whitelisting:SetScript("OnClick", 
             function(self)
-                _G[addonName]["WhitelistChannels"] = ChannelWhitelisting:GetChecked()
+                local isChecked = self:GetChecked()
+                _G[addonName]["WhitelistChannels"] = isChecked
+
+                if isChecked then
+                    showFrames(ChannelWhitelistingDropdown)
+                else
+                    hideFrames(ChannelWhitelistingDropdown)
+                end
+
             end
         )
 
-        -- add/remove players
-
-        -- list of players/channels
-        local player_whitelisting_list_dropdown = CreateFrame("Frame",
-                                                     "WhitelistDropDown",
-                                                     frame,
-                                                     "UIDropDownMenuTemplate")
-        player_whitelisting_list_dropdown:SetPoint("BOTTOMLEFT")
-        UIDropDownMenu_SetWidth(player_whitelisting_list_dropdown, 200)
-        UIDropDownMenu_SetText(player_whitelisting_list_dropdown, "Whitelisted players")
-        UIDropDownMenu_Initialize(player_whitelisting_list_dropdown, addon.populatePlayersDropDown)
-
-        local channel_whitelisting_list_dropdown = CreateFrame("Frame",
-                                                              "WhitelistDropDown",
-                                                              frame,
-                                                              "UIDropDownMenuTemplate")
-        channel_whitelisting_list_dropdown:SetPoint("BOTTOMRIGHT")
-        UIDropDownMenu_SetWidth(channel_whitelisting_list_dropdown, 200)
-        UIDropDownMenu_SetText(channel_whitelisting_list_dropdown, "Whitelisted channels")
-        UIDropDownMenu_Initialize(channel_whitelisting_list_dropdown, addon.populateDropDownChannels)
     end
 )
 
